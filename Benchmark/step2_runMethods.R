cff0d38d2cb655c4fb5daa4f5988bde63a56d1f9
Zhang, Feng <jumphone@163.com>
2019-07-03 23:46:00 -0400

    Update step2_runMethods.R

--- Benchmark/step2_runMethods.R
+++ Benchmark/step2_runMethods.R
@@ -14,8 +14,24 @@ library(limma)
 source('Delia.R')
 ################
 
-# Delia with variable genes 
-mydelia = Delia(REXP, V.SC.REF, COMBAT=TRUE) # 1 second
+
+# Delia with all genes, 2 seconds
+mydelia = Delia(REXP, SC.REF, COMBAT=TRUE) 
+RATIO=mydelia$out
+#######
+saveRDS(RATIO, file='./RESULT/Delia_all.RDS')
+#######
+CORMAT=cor(t(RATIO), t(ALLR), method='pearson')
+pdf('./RESULT/Delia_all.pdf',width=6, height=6)
+library('gplots')
+heatmap.2(CORMAT,scale=c("none"),dendrogram='none',Rowv=F,Colv=F,cellnote=round(CORMAT,2),notecol='black',
+    trace='none',col=colorRampPalette(c('royalblue','grey80','indianred')),margins=c(10,10))
+dev.off()
+#########################
+
+
+# Delia with variable genes, 1 second
+mydelia = Delia(REXP, V.SC.REF, COMBAT=TRUE) 
 RATIO=mydelia$out
 #######
 saveRDS(RATIO, file='./RESULT/Delia_var.RDS')
@@ -29,21 +45,22 @@ dev.off()
 #########################
 
 
-
-# Delia with all genes 
-mydelia = Delia(REXP, SC.REF, COMBAT=TRUE) # 2 seconds
-RATIO=mydelia$out
+# CIBERSORT with variable genes, 229 seconds
+CB=read.table('./RESULT/CIBERSORT.Output_Job1.txt',header=T,row.names=1,sep='\t')
+RATIO=t(CB[,c(1:(ncol(CB)-3))])
 #######
-saveRDS(RATIO, file='./RESULT/Delia_all.RDS')
+saveRDS(RATIO, file='./RESULT/CIBERSORT_var.RDS')
 #######
 CORMAT=cor(t(RATIO), t(ALLR), method='pearson')
-pdf('./RESULT/Delia_all.pdf',width=6, height=6)
+pdf('./RESULT/CIBERSORT_var.pdf',width=6, height=6)
 library('gplots')
 heatmap.2(CORMAT,scale=c("none"),dendrogram='none',Rowv=F,Colv=F,cellnote=round(CORMAT,2),notecol='black',
     trace='none',col=colorRampPalette(c('royalblue','grey80','indianred')),margins=c(10,10))
 dev.off()
 #########################
 
+
+# CIBERSORTx with variable genes, 118 seconds
 
 
 
