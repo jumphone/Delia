b6b0168fe8415ad113555e88e9cc32fc580753a8
Zhang, Feng <jumphone@163.com>
2019-07-03 23:28:40 -0400

    Update step2_runMethods.R

--- Benchmark/step2_runMethods.R
+++ Benchmark/step2_runMethods.R
@@ -1,10 +1,51 @@
-
+# Load Data
 REF=readRDS('./RDS/REF.RDS')
+ALLR=readRDS('./RDS/ALLR.RDS')
 EXP=readRDS('./RDS/EXP.RDS')
 REXP=readRDS('./RDS/REXP.RDS')
 SC.REF=readRDS('./RDS/SC.REF.RDS')
 V.SC.REF=readRDS('./RDS/V.SC.REF.RDS')
 V.SC.REF_scmat=readRDS('./RDS/V.SC.REF_scmat.RDS')
+################
 
-
+# Load Package 
+library(sva)
+library(limma)
 source('Delia.R')
+################
+
+# Delia with variable genes 
+mydelia = Delia(REXP, V.SC.REF, COMBAT=TRUE) # 1 second
+RATIO=mydelia$out
+#######
+saveRDS(RATIO, file='./RESULT/Delia_var.RDS')
+#######
+CORMAT=cor(t(RATIO), t(ALLR), method='pearson')
+pdf('./RESULT/Delia_var.pdf',width=6, height=6)
+library('gplots')
+heatmap.2(CORMAT,scale=c("none"),dendrogram='none',Rowv=F,Colv=F,cellnote=round(CORMAT,2),notecol='black',
+    trace='none',col=colorRampPalette(c('royalblue','grey80','indianred')),margins=c(10,10))
+dev.off()
+#########################
+
+
+
+# Delia with all genes 
+mydelia = Delia(REXP, SC.REF, COMBAT=TRUE) # 2 seconds
+RATIO=mydelia$out
+#######
+saveRDS(RATIO, file='./RESULT/Delia_all.RDS')
+#######
+CORMAT=cor(t(RATIO), t(ALLR), method='pearson')
+pdf('./RESULT/Delia_all.pdf',width=6, height=6)
+library('gplots')
+heatmap.2(CORMAT,scale=c("none"),dendrogram='none',Rowv=F,Colv=F,cellnote=round(CORMAT,2),notecol='black',
+    trace='none',col=colorRampPalette(c('royalblue','grey80','indianred')),margins=c(10,10))
+dev.off()
+#########################
+
+
+
+
+
+
