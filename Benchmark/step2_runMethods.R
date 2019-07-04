ea98c4d982e41e52fb2059493d3908dee88fca19
Zhang, Feng <jumphone@163.com>
2019-07-03 23:19:14 -0400

    Create step2_runMethods.R

---
+++ Benchmark/step2_runMethods.R
@@ -0,0 +1,10 @@
+
+REF=readRDS('./RDS/REF.RDS')
+EXP=readRDS('./RDS/EXP.RDS')
+REXP=readRDS('./RDS/REXP.RDS')
+SC.REF=readRDS('./RDS/SC.REF.RDS')
+V.SC.REF=readRDS('./RDS/V.SC.REF.RDS')
+V.SC.REF_scmat=readRDS('./RDS/V.SC.REF_scmat.RDS')
+
+
+source('Delia.R')
