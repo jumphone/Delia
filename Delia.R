d172f15b47eaf97fa87b95499e8ba752c04cd39d
Zhang, Feng <jumphone@163.com>
2019-07-11 15:05:00 -0400

    Update Delia.R

--- Delia.R
+++ Delia.R
@@ -195,7 +195,7 @@ Delia <- function(EXP, REF, COMBAT=TRUE){
     }
 
 
-.writeTable <- function(DATA, PATH, SEP='\t',TL='GENE'){
+.writeTable <- function(DATA, PATH, SEP='\t',TL='DELIA'){
     DATA=DATA
     PATH=PATH
     SEP=SEP
