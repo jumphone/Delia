e90ea79556cd46b19b47308bdf5c523bd41b39ec
Zhang, Feng <jumphone@163.com>
2019-07-11 22:19:01 -0400

    Update go2.R

--- LAB/go2.R
+++ LAB/go2.R
@@ -1,3 +1,4 @@
+setwd('F:/Delia')
 # Load Data
 REF=readRDS('./RDS/REF.RDS')
 ALLR=readRDS('./RDS/ALLR.RDS')
@@ -7,6 +8,37 @@ SC.REF=readRDS('./RDS/SC.REF.RDS')
 V.SC.REF=readRDS('./RDS/V.SC.REF.RDS')
 V.SC.REF_scmat=readRDS('./RDS/V.SC.REF_scmat.RDS')
 ################
+VAR=apply(SC.REF, 1, var)
+VG=which(rank(-VAR) <= 2000  )
+########################
+
+library(deconvSeq)
+############################
+
+
+sc_mat=read.table('Zeisel_exp_sc_mat.txt',sep='\t',header=T,row.names=1)
+SCMAT=sc_mat[VG,]
+
+colnames(SCMAT)=paste0(colnames(SCMAT),'_',c(1:ncol(SCMAT)))
+tissue.sc = colnames(V.SC.REF_scmat)
+design.sc = model.matrix(~-1+as.factor(tissue.sc))
+colnames(design.sc) = levels(as.factor(tissue.sc))
+rownames(design.sc) = names(tissue.sc)
+dge.sc = getdge(SCMAT, design.sc, ncpm.min=1, nsamp.min=4, method="bin.loess")
+b0.sc = getb0.rnaseq(dge.sc, design.sc, ncpm.min=1, nsamp.min=4)
+##########################################
+
+
+
+####################################################
+dge.tissue = getdge(REXP,design=NULL, ncpm.min=1,nsamp.min=4)
+####################################################
+resultx1_s.sc = getx1.rnaseq(NB0=50, b0.sc, dge.tissue)
+
+
+
+
+
 
 # Load Package 
 library(sva)
@@ -27,6 +59,8 @@ heatmap.2(CORMAT,scale=c("none"),dendrogram='none',Rowv=F,Colv=F,cellnote=round(
     trace='none',col=colorRampPalette(c('royalblue','grey80','indianred')),margins=c(10,10))
 dev.off()
 ########################
+
+
 
 RATIO=readRDS(file='./RESULT/Delia_all.RDS')
 CORMAT=cor(t(RATIO), t(ALLR), method='pearson')
@@ -52,6 +86,41 @@ dev.off()
 RATIO=readRDS(file='./RESULT/Delia_var.RDS')
 CORMAT=cor(t(RATIO), t(ALLR), method='pearson')
 CORMAT[1,1]+CORMAT[2,2]+CORMAT[3,4]+CORMAT[3,5]+CORMAT[4,6]+CORMAT[5,7]
+
+
+
+
+
+
+library(devtools)
+install_github("rosedu1/deconvSeq")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
